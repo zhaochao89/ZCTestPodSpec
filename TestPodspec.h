89f70b1950bd3338cf1f1072fb5732b9829fa099
zhaochao89 <zhaochao0801@sina.com>
2017-04-24 16:13:51 +0800

    第一次提交

---
+++ TestPodspec.h
@@ -0,0 +1,13 @@
+//
+//  TestPodspec.h
+//  TestCoreData
+//
+//  Created by zhaochao on 2017/4/24.
+//  Copyright © 2017年 zhaochao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestPodspec : NSObject
+
+@end
---
+++ TestPodspec.m
@@ -0,0 +1,13 @@
+//
+//  TestPodspec.m
+//  TestCoreData
+//
+//  Created by zhaochao on 2017/4/24.
+//  Copyright © 2017年 zhaochao. All rights reserved.
+//
+
+#import "TestPodspec.h"
+
+@implementation TestPodspec
+//测试空文件
+@end
